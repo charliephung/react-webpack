0c4798d7ce98402957799d2377b97729f40c0c26
charlie <charliephung@gmail.com>
2018-08-10 15:56:32 +0300

    setup react app

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-webpack
+
+## react + webpack + babel boilerplate
+
+### npm install
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const a = import("./Test");
+const a = import("./Test").then(res => {
+  console.log(res);
+});
 
 const App = () => {
   return <div>Hello React!</div>;
